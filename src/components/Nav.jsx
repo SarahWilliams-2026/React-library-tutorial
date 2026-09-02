5663035099fef0d42f460810f85c6c3c689c9620
Sarahhassweetcheeks <Sarahhassweetcheeks@gmail.com>
2026-09-02 09:40:17 -0400

    Stuck on Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import LibraryLogo from '../assets/Library.svg';
+import LibraryLogo from '../Assets/Library.jpg';
 
 const Nav = () => {
     return (
