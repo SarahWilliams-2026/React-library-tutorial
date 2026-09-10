165c8bcfe10c22da0a55d9095717c560741fe883
Sarahhassweetcheeks <Sarahhassweetcheeks@gmail.com>
2026-09-10 13:47:56 -0400

    finished filter but errors

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from './components/Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home';
 import Books from './pages/Books';
+import { books } from './data';
 
 function App() {
   return (
@@ -10,8 +11,8 @@ function App() {
       <div className="App">
         <Nav />
         <Routes>
-          <Route path="/" exact component={Home} />
-          <Route path="/books" component={Books} />
+          <Route path="/" element={<Home />} />
+          <Route path="/books" render={() => <Books books={books} />} />
         </Routes>
         <Footer />
       </div>
