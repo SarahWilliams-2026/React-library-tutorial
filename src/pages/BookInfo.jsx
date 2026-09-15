74c91cc3f60b0e39508231da03e75eefa03bb625
Sarahhassweetcheeks <Sarahhassweetcheeks@gmail.com>
2026-09-15 14:23:56 -0400

    finished add to cart with issue

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useParams } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Rating from '../components/ui/Rating';
 import Price from '../components/ui/Price';
+import Book from '../components/ui/Book'
 
-const BookInfo = ({ books }) => {
+const BookInfo = ({ books, addToCart }) => {
     const { id } = useParams();
-    const book = book.find(book => +book.id === +id);
+    const book = books.find(book => +book.id === +id);
+
+    function addBookToCart(book) {
+        addToCart(book);
+    }
+
+function bookExistsOnCart() {
+    return cart.find((book) => +book.id === +id);
+}
+
     return (
         <div id="books__body">
             <main id="books__main">
@@ -49,9 +59,15 @@ const BookInfo = ({ books }) => {
                                         numquam iusto et assumenda non.
                                     </p>
                                 </div>
-                                <button className="btn">
-                                    Add to cart
-                                </button>
+                                {bookExistsOnCart() ? (
+                                    <Link to={`/cart`} className="book__link">
+                                        <button className="btn">Checkout</button>
+                                    </Link>
+                                ) : (
+                                    <button className="btn" onClick={() => addBookToCart(book)}>
+                                        Add to cart
+                                    </button>
+                                )}
                             </div>
                         </div>
                     </div>
@@ -59,9 +75,16 @@ const BookInfo = ({ books }) => {
                 <div className="books__container">
                     <div className="row">
                         <div className="book__selected--top">
-                            <div className="book__selected--title--top">
-                                Recommended Books
-                            </div>
+                            <h2 className="book__selected--title--top">
+                               Recommended Books
+                            </h2>
+                        </div>
+                        <div className="books">
+                            {books
+                                .filter(book => book.rating === 5 && +book.id !== +id)
+                                .slice(0, 4)
+                                .map(book => <Book book={book} key={book.id} />)
+                            }
                         </div>
                     </div>
                 </div>
